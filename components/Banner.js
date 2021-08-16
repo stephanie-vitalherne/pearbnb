9bbf0460250c1af0d3e371413da97b747a9def6b
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-16 03:02:00 -0400

    banner complete

--- components/Banner.js
+++ components/Banner.js
@@ -8,6 +8,13 @@ function Banner() {
         objectFit="cover"
         src="https://links.papareact.com/0fm"
       />
+
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
+      </div>
     </div>
   );
 }
