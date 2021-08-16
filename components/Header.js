26a4eb3640c8357475dedd807fb912db69971137
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-16 19:46:33 -0400

    date picker has been implemented

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import {
   SearchIcon,
@@ -6,8 +7,31 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [guests, setGuests] = useState(1);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left - Logo */}
@@ -24,6 +48,8 @@ function Header() {
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
           type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           placeholder="Start your search"
           className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400"
         />
@@ -40,6 +66,39 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-2">
+          <DateRangePicker
+            minDate={new Date()}
+            ranges={[selectionRange]}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          {/* Guests */}
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              min={1}
+              type="number"
+              value={guests}
+              onChange={(e) => setGuests(e.target.value)}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+
+          {/* Buttons */}
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Submit</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
