1d16974b460f636ef7ec4ecfa5c07203ccd4d178
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-16 02:05:09 -0400

    logo finished for now

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <header>
-      <h1>Banana</h1>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
-      <div className="">
-        <Image src="https://links.papareact.com/qd3" layout="fill" />
+      <div className="relative flex items=center h-10 cursor-pointer">
+        <Image
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+          src="https://links.papareact.com/qd3"
+        />
       </div>
 
       {/* Middle */}
