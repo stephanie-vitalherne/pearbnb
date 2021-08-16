c731abb743f8a076114a49552e5275aa20f0db39
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-16 01:41:45 -0400

    initial commit - making the navi/header

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div>
+      <h1>Banana</h1>
+      {/* Left */}
+      <div className=""></div>
+
+      {/* Middle */}
+      <div className=""></div>
+
+      {/* Right */}
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default Header;
