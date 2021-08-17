b17d4a211683c51677f5548a9929ff287e74ecc8
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-17 01:02:06 -0400

    implemented the progress bar on the app file so it can be used univerally

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import Router from "next/router";
 import ProgressBar from "@badrap/bar-of-progress";
 
 const progress = new ProgressBar({
@@ -8,6 +9,10 @@ const progress = new ProgressBar({
   className: "z-50",
   delay: 100,
 });
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
