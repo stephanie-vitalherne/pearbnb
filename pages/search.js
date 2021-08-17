20d1457406a811fbf31abb4952e8ea6fe2b8a1b5
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-16 21:17:48 -0400

    created custom styling and a search page

---
+++ pages/search.js
@@ -0,0 +1,27 @@
+import { Header, Footer } from "../components";
+
+function Search() {
+  return (
+    <div>
+      <Header />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays for 5 number of guests</p>
+          <h1 className="text-3xl mt-2 mb-6">Stays in Mars</h1>
+
+          {/* Buttons */}
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
