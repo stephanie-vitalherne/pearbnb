6b3ec0d0ba29a10e15c2ce8bc962c25fd0a3d1a9
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-08-17 00:53:53 -0400

    adding progress bar for top of screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
